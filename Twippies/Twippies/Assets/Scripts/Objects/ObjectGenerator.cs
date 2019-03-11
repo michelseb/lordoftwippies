9b456f6f480d786bd3d6b6053301cec58cf0e174
michelseb <sebastien.michel60@gmail.com>
2019-03-12 00:31:23 +0100

    stats fix

--- Twippies/Twippies/Assets/Scripts/Objects/ObjectGenerator.cs
+++ Twippies/Twippies/Assets/Scripts/Objects/ObjectGenerator.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour {
